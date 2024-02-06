47636353ee8fa8fec9ebd90c2beb1f618cf7f898
Vladimir Moshchuk <ardonplay@gmail.com>
2024-02-06 21:17:53 +0300

    feat: change id's type to uuid

--- init.sql
+++ init.sql
@@ -16,8 +16,13 @@ CREATE TABLE
         "name" varchar UNIQUE NOT NULL
     );
 
+CREATE TABLE "localization_type" (
+  "id" SERIAL PRIMARY KEY,
+  "name" varchar NOT NULL
+);
+
 CREATE TABLE
-    "users" (
+    "user" (
         "id" SERIAL PRIMARY KEY,
         "username" varchar UNIQUE NOT NULL,
         "role" int NOT NULL,
@@ -26,55 +31,50 @@ CREATE TABLE
     );
 
 CREATE TABLE
-    "pages" (
-        "id" SERIAL PRIMARY KEY,
-        "parent_id" int,
+    "page" (
+        "id" uuid NOT NULL DEFAULT (gen_random_uuid()) PRIMARY KEY,
+        "parent_id" uuid,
         "type" int NOT NULL,
         "order_id" int,
         FOREIGN KEY ("type") REFERENCES "page_type" ("id"),
-        FOREIGN KEY ("parent_id") REFERENCES "pages" ("id") ON DELETE SET NULL
+        FOREIGN KEY ("parent_id") REFERENCES "page" ("id") ON DELETE SET NULL
     );
 
 CREATE TABLE
     "page_content" (
-        "id" SERIAL PRIMARY KEY,
+        "id" uuid NOT NULL DEFAULT (gen_random_uuid()) PRIMARY KEY,
         "element_type" int NOT NULL,
         FOREIGN KEY ("element_type") REFERENCES "page_element_type" ("id")
     );
 
 CREATE TABLE
     "page_content_order" (
-        "id" SERIAL PRIMARY KEY,
-        "page_id" int NOT NULL,
-        "content_id" int NOT NULL,
+        "id" uuid NOT NULL DEFAULT (gen_random_uuid()) PRIMARY KEY,
+        "page_id" uuid NOT NULL,
+        "content_id" uuid NOT NULL,
         "order_id" int NOT NULL,
-        FOREIGN KEY ("page_id") REFERENCES "pages" ("id"),
+        FOREIGN KEY ("page_id") REFERENCES "page" ("id"),
         FOREIGN KEY ("content_id") REFERENCES "page_content" ("id") ON DELETE CASCADE
     );
 
 CREATE UNIQUE INDEX "uc_unique_combination_orders" ON "page_content_order" ("page_id", "content_id", "order_id");
 
-CREATE TABLE "localization" (
-  "id" SERIAL PRIMARY KEY,
-  "name" varchar NOT NULL
-);
-
 CREATE TABLE "page_localization" (
-  "id" SERIAL PRIMARY KEY,
-  "page_id" int NOT NULL,
+  "id" uuid NOT NULL DEFAULT (gen_random_uuid()) PRIMARY KEY,
+  "page_id" uuid NOT NULL,
   "language" int NOT NULL,
   "title" varchar NOT NULL,
-  FOREIGN KEY ("page_id") REFERENCES "pages" ("id") ON DELETE CASCADE,
-  FOREIGN KEY ("language") REFERENCES "localization" ("id")
+  FOREIGN KEY ("page_id") REFERENCES "page" ("id") ON DELETE CASCADE,
+  FOREIGN KEY ("language") REFERENCES "localization_type" ("id")
 );
 
 CREATE TABLE "page_content_localization" (
-  "id" SERIAL PRIMARY KEY,
-  "content_id" int NOT NULL,
+  "id" uuid NOT NULL DEFAULT (gen_random_uuid()) PRIMARY KEY,
+  "content_id" uuid NOT NULL,
   "language" int NOT NULL,
   "body" jsonb NOT NULL,
   FOREIGN KEY ("content_id") REFERENCES "page_content" ("id") ON DELETE CASCADE,
-  FOREIGN KEY ("language") REFERENCES "localization" ("id")
+  FOREIGN KEY ("language") REFERENCES "localization_type" ("id")
 );
 
 
@@ -107,17 +107,25 @@ INSERT INTO "page_element_type" ("name") VALUES ('IMAGE'), ('TEXT');
 
 INSERT INTO "user_role" ("name") VALUES ('ROLE_ADMIN');
 
-INSERT INTO "localization" ("name") VALUES ('EN'), ('RU'), ('BY');
+INSERT INTO "localization_type" ("name") VALUES ('EN'), ('RU'), ('BY');
 
 INSERT INTO
-    "pages" (
+    "page" (
         "parent_id",
         "type",
         "order_id"
     )
-VALUES (NULL, 1, 1), (1, 2, 1);
+VALUES (NULL, 1, 1);
 
-INSERT INTO "page_localization" ("page_id", "language", "title") VALUES (1, 1, 'TEST_FOLDER'), (2, 1, 'TEST_PAGE');
+INSERT INTO
+    "page" (
+        "parent_id",
+        "type",
+        "order_id"
+    )
+VALUES ((SELECT page.id FROM "page" LIMIT 1 OFFSET 0), 2, 1);
+
+INSERT INTO "page_localization" ("page_id", "language", "title") VALUES ((SELECT page.id FROM "page" LIMIT 1 OFFSET 0), 1, 'TEST_FOLDER'), ((SELECT page.id FROM "page" LIMIT 1 OFFSET 1), 1, 'TEST_PAGE');
 
 INSERT INTO
     "page_content" (
@@ -128,10 +136,10 @@ VALUES (
     );
 
 
-INSERT INTO "page_content_localization" ("content_id", "language", "body") VALUES (1, 1,  '{"content": "Hello, this is test"}':: jsonb);
+INSERT INTO "page_content_localization" ("content_id", "language", "body") VALUES ((SELECT id FROM "page_content" LIMIT 1 OFFSET 0), 1,  '{"content": "Hello, this is test"}':: jsonb);
 
 INSERT INTO "page_content_order" ("page_id", "content_id", "order_id")
 VALUES
 (
-    2, 1, 1
+    (SELECT page.id FROM "page" LIMIT 1 OFFSET 1), (SELECT id FROM "page_content" LIMIT 1 OFFSET 0), 1
 )
